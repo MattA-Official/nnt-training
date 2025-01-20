d19eed4ee3b4f0febd0cead77fbdfc4225a77df8
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-01-20 23:05:07 +0000

    Add Firebase configuration and dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-vuefire'],
+
+  vuefire: {
+    config: { // This will need replacing with the Firebase config for your project
+      apiKey: "AIzaSyDtXd4n9crDStw6KbXmvQ7wSQBJpcR-8vk",
+      authDomain: "nnt-training-test.firebaseapp.com",
+      projectId: "nnt-training-test",
+      storageBucket: "nnt-training-test.firebasestorage.app",
+      messagingSenderId: "560599823624",
+      appId: "1:560599823624:web:0a61c02269e3e39f65e8bd"
+    },
+
+  },
+
+  nitro: {
+    firebase: {
+      gen: 2
+    }
+  }
 })
